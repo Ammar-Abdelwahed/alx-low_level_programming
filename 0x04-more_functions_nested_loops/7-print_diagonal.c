e5273cc26d05cfffb52c5a4b5ef1aaf000f2a45f
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-12-31 15:09:45 -0800

    more c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal straight line
+ * @n: the length of the line
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
